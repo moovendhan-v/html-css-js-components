65c4b126098698b55a88352ca6850529723964ac
agricreations <moovendhanagriculture@gmail.com>
2024-01-13 09:55:06 +0530

    hero section scrool animation added

--- ui/src/components/Hero.jsx
+++ ui/src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import TypingTextEffect from './TypingText';
 import SeachBar from './Search';
+import ScroolAnimation from './Scrool';
+import { ScrollAnimationForImage } from "./Scrool";
 
 function Hero(){
     return(
@@ -9,12 +11,14 @@ function Hero(){
             <div className="hero_section text-center">
                 <h3 className="fw-bold lh-base">Open Source Ui-Componenets <br /> for your next projects</h3>
             </div>
-            <div className="hero_info text-center my-5 fw-bolder ">
+            <div className="hero_info text-center my-3 fw-bolder ">
                 {/* <p>Save and share your creative designes to open source</p> */}
                 <TypingTextEffect  text="Save and share your creative designes to open source" delay={100}  />
             </div>
             <div className="hero_search">
                 <SeachBar />
+                <ScroolAnimation />
+                <ScrollAnimationForImage />
             </div>
         </section>
         </>
---
+++ ui/src/components/Scrool.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ScrollAnimation = () => {
+  const items = ["First", "Second", "Third", "Fourth", "Fifth","First", "Second", "Third", "Fourth", "Fifth"];
+  const duplicatedItems = items.map((item, index) => (
+    <li className="btn" key={index}>
+      {item}
+      <span aria-hidden="true">{item}</span>
+    </li>
+  ));
+  return (
+    <>
+      <div id="scroolcontainer" className="text-center py-4" data-animated>
+        <ul id="scrool_list" >{duplicatedItems}</ul>
+      </div>
+    </>
+  );
+};
+
+const ScrollAnimationForImage = () => {
+    const imgs = "https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const itemss = [imgs,imgs,imgs,imgs,imgs];
+    const duplicatedItems = itemss.map((item, index) => (
+      <li className="btn" key={index}>
+        <img className="img-fluid" src={item} alt="" />
+      </li>
+    ));
+    return (
+      <>
+        <div id="scroolcontainer" className="text-center py-4" data-animated>
+          <ul id="scrool_list" >{duplicatedItems}</ul>
+        </div>
+      </>
+    );
+  };
+
+
+export default ScrollAnimation;
+export { ScrollAnimationForImage };
