0c3d6a7dad027fa50cfb8a1bbe83cceb5930020e
agricreations <moovendhanagriculture@gmail.com>
2024-02-21 07:58:07 +0530

    removing unwanted code

--- ui/src/components/Scrool.jsx
+++ ui/src/components/Scrool.jsx
@@ -18,18 +18,6 @@ const ScrollAnimation = () => {
 };
 
 const ScrollAnimationForImage = ({direction}) => {
-
-    const imgs = "https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-    const itemss = [imgs, imgs, imgs, imgs, imgs];
-
-    const duplicatedItems = itemss.map((item, index) => (
-        <div className="sliderImg">
-            <li key={index}>
-                <img data-animated="true" className="img-fluid" src={item} alt="" aria-hidden="true"/>
-            </li>
-        </div>
-
-    ));
     return (
         <>
             <div id={`scroolcontainer${direction}`} className="text-center py-2" data-animated>
@@ -37,7 +25,6 @@ const ScrollAnimationForImage = ({direction}) => {
                         <ComponentsCard catogreise="buttons" componentType="scrool" />
                  </ul>
              </div>
-
         </>
     );
 };
