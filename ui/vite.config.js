9fc2582bfc0f6656187148cf02b20b2ed9a318fd
agricreations <moovendhanagriculture@gmail.com>
2024-02-22 17:27:29 +0000

    typo fix in deployment

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'html-css-js-components/ui/', // Set your base URL if your site is hosted in a subdirectory, otherwise, use '/'
+  // base: '/your-base-url/', // Set your base URL if your site is hosted in a subdirectory, otherwise, use '/'
   build: {
     outDir: 'build', // Specify the output directory for the production build
     emptyOutDir: true, // Ensure that the output directory is empty before building
