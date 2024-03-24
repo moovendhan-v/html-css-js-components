72737e6a77d0f3be5e2402357a56dddaa1d6ed3d
agricreations <moovendhanagriculture@gmail.com>
2024-03-24 22:20:46 +0530

    using Nav of shadecnm

--- ui_tailwind_shadecn/src/App.tsx
+++ ui_tailwind_shadecn/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import '../app/globals.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './screens/Home';
+import {Dashboard} from './screens/Home';
 import Testing from './screens/Testing';
 
 
@@ -12,7 +12,7 @@ function App() {
 
 <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/testing" element={<Testing />} />
 
           {/* <Route path="/contributenew" element={<ContributeNewComp />} /> */}
