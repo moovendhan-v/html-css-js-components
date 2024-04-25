977bb9da843de48bd90da1dcc707beace2678cfb
agricreations <moovendhanagriculture@gmail.com>
2024-04-25 08:22:30 +0530

    update view components state with zustand and react state management

--- ui_tailwind_shadecn/src/api/components/component.tsx
+++ ui_tailwind_shadecn/src/api/components/component.tsx
@@ -1,7 +1,7 @@
 // http://localhost:4000/componentsbuttons/moovendhan_amazing_tooltip4
 
 
-// import { useComponentsStore } from '@/store/store';
+import { useViewComponentStore } from '@/store/store';
 // import {ComponentData} from '@/types/ComponentData.type'
 import {getEnvVariable} from '@/utils/load.utils';
 
@@ -20,11 +20,7 @@ export const fetchComponentStore = async (categorie: string , title: string) => 
         const responseData = await response.json();
         const postDetails = responseData.response.post_details; 
         
-        // Extract post_details from response
-        // const details: ComponentData[] = responseData.map((item: ResponseItem) => item.post_details);
-
-        // Update the store
-        // useComponentsStore.setState({ [categorie]: details });
+        useViewComponentStore.setState({viewComponents: postDetails});
 
         return postDetails;
     } catch (error) {
