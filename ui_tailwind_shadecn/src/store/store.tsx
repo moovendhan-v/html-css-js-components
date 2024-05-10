533ec3976897cdfb8cb5a04da698ec09dce6ee92
agricreations <moovendhanagriculture@gmail.com>
2024-05-10 02:48:26 +0000

    Updating typescript depends on api response

--- ui_tailwind_shadecn/src/store/store.tsx
+++ ui_tailwind_shadecn/src/store/store.tsx
@@ -1,56 +1,56 @@
-import {ComponentsStore, ViewComponentStore, } from '@/types/ComponentStore.type'
-import {CategoriesStore} from '@/types/CategoriesStore.type'
+import { ComponentsStore, ViewComponentStore, } from '@/types/ComponentStore.type'
+import { CategoriesStore } from '@/types/CategoriesStore.type'
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 
 
 // Create the store with zustand
 export const useCategoriesStore = create<CategoriesStore>()(
-  persist(
-    (set) => ({
-      categories: [],
-      addCategories: (newCategories) => set((state) => ({
-        categories: [...state.categories, ...newCategories],
-      })),
-    }),
-    {
-      name: 'catogries-store', 
-      storage: createJSONStorage(() => sessionStorage), 
-      partialize: (state) => ({ categories: state.categories}),
-    }
-  )
+    persist(
+        (set) => ({
+            categories: [],
+            addCategories: (newCategories) => set((state) => ({
+                categories: [...state.categories, ...newCategories],
+            })),
+        }),
+        {
+            name: 'catogries-store',
+            storage: createJSONStorage(() => sessionStorage),
+            partialize: (state) => ({ categories: state.categories }),
+        }
+    )
 );
 
 export const useComponentsStore = create<ComponentsStore>(() => ({
-    all : [],
-    navbar : [],
-    buttons : [],
-    carosel : [],
-    hero_section : [],
-    dropdown : [],
-    testimonial : [],
-    features : [],
-    toggles : [],
-    faq : [],
-    forms : [],
-    pricing : [],
-    login_signup : [],
-    accordian : [],
-    cards : [],
-    toggle : [],
-    tabs : [],
-    input : [],
-    modals : [],
-    notification : [],
-    loader : [],
-    countdown : [],
-    contactus : [],
-    footer : [],
-    tooltip : [],
-    others : [],
-    checkbox : [],
-    gradient : [],
-    toast : [],
+    all: [],
+    navbar: [],
+    buttons: [],
+    carosel: [],
+    hero_section: [],
+    dropdown: [],
+    testimonial: [],
+    features: [],
+    toggles: [],
+    faq: [],
+    forms: [],
+    pricing: [],
+    login_signup: [],
+    accordian: [],
+    cards: [],
+    toggle: [],
+    tabs: [],
+    input: [],
+    modals: [],
+    notification: [],
+    loader: [],
+    countdown: [],
+    contactus: [],
+    footer: [],
+    tooltip: [],
+    others: [],
+    checkbox: [],
+    gradient: [],
+    toast: [],
     search: [],
 }));
 
@@ -59,35 +59,35 @@ export const useViewComponentStore = create<ViewComponentStore>(() => ({
         html: "",
         css: "",
         js: "",
-        catogries: "",
+        categories: "",
         folder_path: "",
         folder_name: "",
         isActive: false,
         type: "",
         like: {
-          isLiked: false,
-          likeCount: "0"
+            isLiked: false,
+            likeCount: 0
         },
-        saved:{
-          isSaved: false,
-          savedCount: "0"
+        saved: {
+            isSaved: false,
+            savedCount: 0
         },
-        views:{
-          count: ""
+        views: {
+            count: 0
         },
         title: "",
         description: "",
-        comments:{
-            count: "",
-            commentsList:[
-              {
-                comment:"",
-                user:"",
-                avatar:"",
-                date:""
-              }
+        comments: {
+            count: 0,
+            commentsList: [
+                {
+                    comment: "",
+                    user: "",
+                    avatar: "",
+                    date: ""
+                }
             ]
-          },
+        },
         admin: {
             _id: "",
             login: "",
@@ -96,12 +96,10 @@ export const useViewComponentStore = create<ViewComponentStore>(() => ({
             html_url: "",
             company: "",
             location: "",
-            email: null,
             name: "",
             blog: "",
             bio: "",
             twitter_username: null,
-            __v: 0,
         },
     },
 }));
--- ui_tailwind_shadecn/src/types/ComponentData.type.tsx
+++ ui_tailwind_shadecn/src/types/ComponentData.type.tsx
@@ -4,49 +4,47 @@ export interface Comment {
   avatar: string;
   user: string;
   comment: string;
+  date: string;
 }
 
 
 export interface ComponentData {
-    html: string;
-    css: string;
-    js: string;
-    catogries: string;
-    folder_path: string;
-    folder_name: string;
-    isActive: boolean;
-    type: string;
-    like: {
-      isLiked: boolean | false;
-      likeCount:string | "0";
-    };
-    views:{
-      count: string;
-    };
-    saved:{
-      isSaved: boolean | false;
-      savedCount: string | "0";
-    };
-    title: string;
-    description: string;
-    comments:{
-      count: string,
-      commentsList:[
-        Comment
-      ]
-    };
-    admin: {
-      _id: string;
-      login: string;
-      avatar_url: string;
-      url: string;
-      html_url: string;
-      company: string;
-      location: string;
-      email: string | null;
-      name: string;
-      blog: string;
-      bio: string;
-      twitter_username: string | null;
-    };
-  }
+      html: string;
+      css: string;
+      js: string;
+      categories: string;
+      folder_path: string;
+      folder_name: string;
+      isActive: boolean;
+      type: string;
+      like: {
+          isLiked: boolean;
+          likeCount: number;
+      };
+      saved: {
+          isSaved: boolean;
+          savedCount: number;
+      };
+      views: {
+          count: number;
+      };
+      title: string;
+      description: string;
+      comments: {
+          count: number;
+          commentsList: [Comment];
+      };
+      admin: {
+          _id: string;
+          login: string;
+          avatar_url: string;
+          url: string;
+          html_url: string;
+          company: string;
+          location: string;
+          name: string;
+          blog: string;
+          bio: string;
+          twitter_username: string | null;
+      };
+}
